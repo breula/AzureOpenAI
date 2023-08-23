480ca9b12d56078ce8829c0c878892c21f17dab3
Marcio Viterbo <marcio@soaresviterbo.com.br>
2023-08-23 14:23:32 -0300

    dicionado testes unitários.

---
+++ AzureOpenAITests/ProgramTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureOpenAI.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ConsoleInputIsValidRangeTest()
+        {
+            // arrange
+            List<KeyValuePair<int, string>> options = new() {
+                new KeyValuePair<int, string>(1,"Completion"),
+                new KeyValuePair<int, string>(2,"Chat Completion"),
+                new KeyValuePair<int, string>(3,"Embeddings")
+            };
+            int n = 1;
+
+            // assert
+            Assert.IsTrue(n >= 1 && n <= options.Count);
+        }
+
+        [TestMethod()]
+        public void ConsoleInputIsValidParamTest()
+        {
+            // arrange
+            object? value = null;
+
+            // assert
+            if (value == null)
+            {
+                Assert.IsTrue(value == null, "If object parameter is null always return FALSE");
+            }
+            else
+            {
+                var isNumeric = int.TryParse((string?)value, out int n);
+                if (isNumeric)
+                {
+                    Assert.IsTrue(isNumeric, "If param is number, than showld be a numeral character");
+                }
+                else
+                {
+                    Assert.IsTrue(value == "Y" || value == "N");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GetCompletionResponsePromptEmptyTest()
+        {
+            // arrange
+            string prompt = "SS";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(prompt), "User query cann't be null or empty");
+        }
+
+        [TestMethod()]
+        public void GetCompletionResponseDeploymentNameEmptyTest()
+        {
+            // arrange
+            string deploymentName = "gpt-35-turbo";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(deploymentName), "Deplyment name cann't be null or empty");
+        }
+
+        [TestMethod()]
+        public void GetChatCompletionsResponsePromptEmptyTest()
+        {
+            // arrange
+            string prompt = "SS";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(prompt), "User query cann't be null or empty");
+        }
+
+        [TestMethod()]
+        public void GetChatCompletionsResponseDeploymentNameEmptyTest()
+        {
+            // arrange
+            string deploymentName = "gpt-35-turbo";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(deploymentName), "Deplyment name cann't be null or empty");
+        }
+
+        [TestMethod()]
+        public void GetEmbeddingsResponsePromptEmptyTest()
+        {
+            // arrange
+            string prompt = "SS";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(prompt), "User query cann't be null or empty");
+        }
+
+        [TestMethod()]
+        public void GetEmbeddingsResponseDeploymentNameEmptyTest()
+        {
+            // arrange
+            string deploymentName = "text-embedding-ada-00";
+
+            // assert
+            Assert.IsFalse(string.IsNullOrEmpty(deploymentName), "Deplyment name cann't be null or empty");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Azure;
 
 namespace AzureOpenAI {
 
-    internal class Program
+    public class Program
     {
         private static readonly OpenAIClient _client =
             new(new Uri(Constants.RESOURCE_ENDPOINT), new AzureKeyCredential(Constants.API_KEY));
@@ -11,14 +11,14 @@ namespace AzureOpenAI {
         private static List<KeyValuePair<int, string>> _options = new() { 
             new KeyValuePair<int, string>(1,"Completion"),
             new KeyValuePair<int, string>(2,"Chat Completion"),
-            new KeyValuePair<int, string>(3,"Embeddings"),
-            new KeyValuePair<int, string>(4,"Langchain")
+            new KeyValuePair<int, string>(3,"Embeddings")
         };
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Azure OpenAI by C#. Ten thousand times easier than python!");
-            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("- Wellcome to Azure OpenAI by C#. Ten thousand times easier than python! -");
+            Console.WriteLine("--------------------------------------------------------------------------");
             Console.WriteLine("Choose an option bellow:");
             WriteOptions();
             var option = Console.ReadLine();
@@ -40,7 +40,6 @@ namespace AzureOpenAI {
                     WriteOptions();
                     option = Console.ReadLine();
                 }
-               //LoopPropmpt(Convert.ToInt32(option));
             }
             LoopPropmpt(Convert.ToInt32(option));
 
@@ -48,7 +47,7 @@ namespace AzureOpenAI {
         }
 
         #region-- Utils --
-        private static void LoopPropmpt(int option)
+        public static void LoopPropmpt(int option)
         {
             Console.Write("Your query?: ");
             var prompt = Console.ReadLine();
@@ -71,6 +70,11 @@ namespace AzureOpenAI {
                 }
             }
 
+            RedirectToAction(option, prompt);
+        }
+
+        private static void RedirectToAction(int option, string? prompt)
+        {
             if (option == 1)
             {
                 GetCompletionResponse(prompt);
@@ -79,9 +83,13 @@ namespace AzureOpenAI {
             {
                 GetChatCompletionsResponse(prompt);
             }
+            else if (option == 3)
+            {
+                GetEmbeddingsResponse(prompt);
+            }
         }
 
-        private static void WriteOptions()
+        public static void WriteOptions()
         {
             foreach (var option in _options)
             {
@@ -90,13 +98,54 @@ namespace AzureOpenAI {
             Console.Write("Whitch one?: ");
         }
 
-        private static bool ConsoleInputIsValid(object? value)
+        public static bool ConsoleInputIsValid(object? value)
         {
             if (value == null) return false;
             var isNumeric = int.TryParse((string?)value, out int n);
             return isNumeric 
                 ? n >= 1 && n <= _options.Count
                 : ((string)value).ToUpper() == "Y" || ((string)value).ToUpper() == "N";
+        }
+
+        private static void Retry()
+        {
+            Console.Write("Would you like to try again [Y/N]? If no, application will close.");
+            var yesNo = Console.ReadLine();
+            while (!ConsoleInputIsValid(yesNo))
+            {
+                Console.WriteLine("Please, inform Y or N");
+                Console.Write("Would you like to try again [Y/N]?");
+                yesNo = Console.ReadLine();
+            }
+            if (yesNo.ToUpper() == "Y")
+            {
+                WriteOptions();
+                var option = Console.ReadLine();
+                while (!ConsoleInputIsValid(option))
+                {
+                    if (string.IsNullOrEmpty(option))
+                    {
+                        Console.WriteLine("Please, option can't be null. Choose one:");
+                        WriteOptions();
+                        option = Console.ReadLine();
+                    }
+                    else
+                    {
+                        var isNumeric = int.TryParse(option, out int n);
+                        if (!isNumeric || n > _options.Count)
+                        {
+                            Console.WriteLine($"Wrong option! Please provide a number between 1 and {_options.Count}");
+                        }
+                        WriteOptions();
+                        option = Console.ReadLine();
+                    }
+                }
+                LoopPropmpt(Convert.ToInt32(option));
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
         #endregion
 
@@ -109,6 +158,10 @@ namespace AzureOpenAI {
                 Response<Completions> completionsResponse = _client.GetCompletions(deploymentName, prompt);
                 string completion = completionsResponse.Value.Choices[0].Text;
                 Console.WriteLine($"AI Response: {completion}");
+                Console.WriteLine($"Total Tokens: {completionsResponse.Value.Usage.TotalTokens}");
+                Console.WriteLine();
+
+                Retry();
             }
             catch (Exception e)
             {
@@ -128,10 +181,39 @@ namespace AzureOpenAI {
                     new ChatMessage(ChatRole.Assistant, "Nascer novamente deverá. Burro de mais nesta vida você é!"),
                     new ChatMessage(ChatRole.User, prompt)
                 };
-                var bbb = new ChatCompletionsOptions(tt);
-                Response<ChatCompletions> completionsResponse = _client.GetChatCompletions(deploymentName, bbb);
+                var query = new ChatCompletionsOptions(tt);
+                Response<ChatCompletions> completionsResponse = _client.GetChatCompletions(deploymentName, query);
                 string completion = completionsResponse.Value.Choices[0].Message.Content;
                 Console.WriteLine($"AI Response: {completion}");
+                Console.WriteLine($"Total Tokens: {completionsResponse.Value.Usage.TotalTokens}");
+                Console.WriteLine();
+
+                Retry();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                throw;
+            }
+        }
+
+        private static void GetEmbeddingsResponse(string prompt)
+        {
+            try
+            {
+                string deploymentName = "text-embedding-ada-002";
+                var query = new EmbeddingsOptions(prompt);
+                Response<Embeddings> completionsResponse = _client.GetEmbeddings(deploymentName, query);
+                List<float> completion = (List<float>)completionsResponse.Value.Data[0].Embedding;
+                Console.WriteLine("AI Response:");
+                for (int i = 0; i < completion.Count; i++)
+                {
+                    Console.WriteLine($"{i+1:00000} - {completion[i]}");
+                }
+                Console.WriteLine($"Total Tokens: {completionsResponse.Value.Usage.TotalTokens}");
+                Console.WriteLine();
+
+                Retry();
             }
             catch (Exception e)
             {
